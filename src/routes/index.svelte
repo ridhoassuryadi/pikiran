68181ff0dc534d44905c9e6fe0f10274a38fb2b5
ridho muhammad <mridho@ruangguru.com>
2021-04-25 14:19:13 +0800

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,57 @@
+<script>
+	import successkid from 'images/successkid.jpg';
+</script>
+
+
+<svelte:head>
+	<title>Ridho - Say hi!</title>
+</svelte:head>
+
+<section id="information">
+   <header>
+      <h1>M Ridho Assuryadi</h1>
+      <p>Software Engineer</p>
+   </header>
+   <article>
+      <h2>About</h2>
+      <p>I'm a New Zealand-born, multi-disciplinary designer, with over 9 years experience.</p>
+      <p>Right now I'm based in Australia. But over the last decade I've spent time living in Japan and the United States.</p>
+   </article>
+   <article>
+      <h2>Work</h2>
+      <p>Presently I'm leading a remote team, as Director of Design, at <a href="https://www.rainforestqa.com/" target="_blank">Rainforest</a>. 
+         Before that I worked across product and design teams at 
+         <a href="https://www.getmoneytree.com/" target="_blank">Moneytree</a>, 
+         <a href="https://www.techinasia.com/mercari-acquires-origami/" target="_blank">Origami</a>, 
+         and <a href="https://www.facebook.com/" target="_blank">Facebook</a>.
+      </p>
+      <p>For more details you can find the latest version of <a href="https://www.figma.com/proto/4WuCCKH0BYs1djez3imrJK/Resume?node-id=3%3A759&amp;viewport=75%2C265%2C0.0517074279487133&amp;scaling=contain" target="_blank">my resume here</a>.
+      </p>
+   </article>
+   <article>
+      <h2>Interests</h2>
+      <p>Things I spend my time reading over, tinkering with and learning about.</p>
+      <ul>
+         <li>Open-Source Technologies</li>
+         <li>Peer-to-Peer Communication</li>
+         <li>Cryptography</li>
+         <li>Game Design / Theory</li>
+         <li>ECMAScript</li>
+      </ul>
+   </article>
+   <article id="projects">
+      <h2>Projects</h2>
+      <p>A few ideas that I've gone ahead and built in my spare time. You can support them if you like by <a href="https://liberapay.com/jonathontoon/" target="_blank">donating here</a>.</p>
+      <ul>
+         <li><a href="https://github.com/jonathontoon/manifest/" target="_blank">Manifest</a> — An incredibly simple grid-based pinboard for note taking.</li>
+         <li><a href="https://github.com/jonathontoon/tweet-tray/" target="_blank">Tweet Tray</a> — Compose tweets quickly from the desktop without any distractions.</li>
+      </ul>
+   </article>
+   <article>
+      <h2>Contact</h2>
+      <p>I'd be happy to write back if you'd like to send me an <a href="mailto:hi@jonathontoon.com" target="_blank">email</a>. Otherwise you can follow me on <a href="https://twitter.com/jonathontoon/" target="_blank">Twitter</a>, <a href="https://github.com/jonathontoon/" target="_blank">Github</a> or <a href="https://linkedin.com/in/jonathontoon/" target="_blank">LinkedIn</a>.</p>
+   </article>
+   <footer>
+      <p>The information on this web page was last updated Monday, March 15, 2021. Content provided under <a href="https://creativecommons.org/licenses/by-nc/3.0/" target="_blank">CC BY-NC 3.0</a>.</p>
+   </footer>
+</section>
